8747db92ae543c0734eb2fe81e9d9936b94d820b
szSo <szymon777@o2.pl>
2021-11-17 14:04:52 +0100

    +dodane wypisywanie kaskadowe macierzy -komentarze

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -20,35 +20,13 @@ long long ciagFibonacciego(int n)
                 a = b - a; 
             
         }
-    
-    
     return zwracanaSuma;
 }
-
-
-
-
-//Dzialajacy kod 
-//long long ciagFibonacciego(int n)
-//{
-//    long long a = 0, b = 1, zwracanaSuma = 0;
-//
-//    for (int i = 0; i < n; i++) {
-//        zwracanaSuma += b;
-//        b = b + a;                
-//        a = b - a; 
-//
-//    }
-//
-//
-//    return zwracanaSuma;
-//}
-
 
 template <class T>
 class CMatrix
 {
-public:
+public: 
     CMatrix(int rows, int cols)
     {
         
@@ -66,15 +44,16 @@ public:
                 data[i][j] = 0; 
             }
         }
-    }
+    }   
 
+    friend ostream& operator<<(ostream& output, CMatrix& macierzA);
     void print();
     void setRow(int r) { row = r; }
     void setCol(int c) { col = c; }
     T& operator()(int row, int col);
     CMatrix& operator+(CMatrix m);
     CMatrix& operator*(CMatrix m);
-  
+ 
 
     CMatrix matPow(CMatrix x, long long k) {
 
@@ -96,16 +75,25 @@ public:
             }
         }
         return ans;
-    };
-
+    }
 
 private:
     T** data;
     int row, col;
 
-
 };
 
+ostream& operator<<(ostream& output, CMatrix<long long>& macierz) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            output << " " << macierz.data[i][j];
+        }
+        output << endl;
+    }
+    //output << endl;
+
+    return output;
+}
 
 template <class T>
 void CMatrix<T>::print()
@@ -132,7 +120,7 @@ template<class T> CMatrix<T> &CMatrix<T>::operator+(CMatrix<T> m)
 {
     for (int i = 0; i < row; ++i) {
         for (int j = 0; j < col; ++j) {
-            data[i][j] = (data[i][j] + m.data[i][j]) % mod; //
+            data[i][j] = (data[i][j] + m.data[i][j]) % mod;
         }
     }
     return *this;
@@ -157,31 +145,20 @@ template<class T> CMatrix<T>& CMatrix<T>::operator*(CMatrix<T> m)
     return (*this = temp);
 }
 
-
-
-//TODO: kaskadowe wypisanie maczierzy, n.p. 3 obok siebie
-
-
-
 #endif 
 
-
-
 void print(vector<long long> lVector) {
-   
+
     cout << "\n";
     for (int i = 0; i < lVector.size(); i++)
     {
-        cout << lVector[i]<<"\n";
+        cout << lVector[i] << "\n";
     }
 
 }
 
-//otrzymana liczba w wyniku operacji fibonacci(Indeks ciagu S) % modulo M daja wartosc sumy wyrazow ciagu fibonacciego do przekazanego funkcji indeksu
-//C == 1, czyli ciag An ma kolejne wartosci jak ciag fibonacciego  liczba M to modulo, liczba T ile wyrazow ciagu An(ciagu fibonacciego) 
 int main()
 {
-
    int t;
 
    scanf_s("%lld %lld", &c, &mod);
@@ -190,7 +167,29 @@ int main()
    long long number;
    vector<long long> answers(t);
 
-  
+   CMatrix<long long> m(3, 3);
+   m(0, 0) = c;
+   m(0, 1) = 1;
+   m(0, 2) = 1;
+   m(1, 0) = 1;
+   m(1, 1) = 0;
+   m(1, 2) = 0;
+   m(2, 0) = 0;
+   m(2, 1) = 0;
+   m(2, 2) = 1;
+
+   CMatrix<long long> m1(3, 3);
+   m1(0, 0) = ciagFibonacciego(2);
+   m1(0, 1) = 0;
+   m1(0, 2) = 0;
+   m1(1, 0) = ciagFibonacciego(1);
+   m1(1, 1) = 0;
+   m1(1, 2) = 0;
+   m1(2, 0) = 2 - c;
+   m1(2, 1) = 0;
+   m1(2, 2) = 0;
+
+   CMatrix<long long> mWynik(3, 3);
 
    for (int  i = 0; i < t; i++)
    {
@@ -205,88 +204,18 @@ int main()
            continue;
        }
 
-
-
-       CMatrix<long long> m(3, 3);
-       m(0, 0) = c;
-       m(0, 1) = 1;
-       m(0, 2) = 1;
-       m(1, 0) = 1;
-       m(1, 1) = 0;
-       m(1, 2) = 0;
-       m(2, 0) = 0;
-       m(2, 1) = 0;
-       m(2, 2) = 1;
-
-       CMatrix<long long> m1(3, 3);
-       m1(0, 0) = ciagFibonacciego(2);
-       m1(0, 1) = 0;
-       m1(0, 2) = 0;
-       m1(1, 0) = ciagFibonacciego(1);
-       m1(1, 1) = 0;
-       m1(1, 2) = 0;
-       m1(2, 0) = 2 - c;
-       m1(2, 1) = 0;
-       m1(2, 2) = 0;
-
-
-       CMatrix<long long> mWynik(3, 3);
-       mWynik = m.matPow(m, number-1) * m1;
+       mWynik = m.matPow(m, number - 1) * m1;
 
        answers[i] = mWynik(1, 0);
-
-   }
-
-  
-  print(answers);
+   } 
    
 
-    
-    //CMatrix<long long> m(3, 3);
-    //m(0, 0) = c;
-    //m(0, 1) = 1;
-    //m(0, 2) = 1;
-    //m(1, 0) = 1;
-    //m(1, 1) = 0;
-    //m(1, 2) = 0;
-    //m(2, 0) = 0;
-    //m(2, 1) = 0;
-    //m(2, 2) = 1;
-    //m.print();
-    //cout << "\n";
-    //CMatrix<long long> m1(3, 3);
-    //cout << "\n";
-    //m1(0, 0) = ciagFibonacciego(2);
-    //m1(0, 1) = 0;
-    //m1(0, 2) = 0;
-    //m1(1, 0) = ciagFibonacciego(1);
-    //m1(1, 1) = 0;
-    //m1(1, 2) = 0;
-    //m1(2, 0) = 2 - c;
-    //m1(2, 1) = 0;
-    //m1(2, 2) = 0;
-    //m1.print();
-    //CMatrix<long long> mWynik(3, 3);
+   print(answers);
   
+   cout << "\n"<< "Macierze:"<<"\n";
 
-    //cout << "\n";
-    ////m(1, 1) = 2;
-    ////m = m + m1;
-    //
-    //mWynik = m.matPow(m,8) * m1;
-    //mWynik.print();
-    ////TEst
-    //std::cout << "\nWartosc S10 " << mWynik(0,0)%mod;
-    //std::cout<<"\nWartosc S10 "<<ciagFibonacciego(10);
-
-
-
-    //m.print();
-   // cout << m(1,1);
-    
-    
-
-    
+   cout << m << " *\n"<< m1 << " =\n"<< mWynik;
+  
 }
 
 
